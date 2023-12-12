6bb7ecc4f1c2020bb0a3625dde2f8a5bb20966d0
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-12 11:16:39 -0700

    9 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+
+let mondayWork = function(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`);
+}
+
+let wrapAdjective = function(type = "*"){
+    return function(parameter = "special"){
+        return(`You are ${type}${parameter}${type}!`);
+    }
+}
+    
+
